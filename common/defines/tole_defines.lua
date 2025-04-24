7a858fb27fcf814a9787571b5bf1019b31a40716
Berkem <berkem_elgul@hotmail.com>
2025-04-24 17:27:47 +0300

    definelar

--- common/defines/tole_defines.lua
+++ common/defines/tole_defines.lua
@@ -1,1 +1,118 @@
+NDefines.NGame.COMBAT_LOG_MAX_MONTHS = 6
+NDefines.NResistance.GARRISON_LOG_MAX_MONTHS = 3
+NDefines.NGame.LAG_DAYS_FOR_LOWER_SPEED = 100
+NDefines.NGame.LAG_DAYS_FOR_PAUSE = 300
 NDefines.NGame.GAME_SPEED_SECONDS = { 2.0, 0.25, 0.2, 0.04, 0.0 } -- game speeds for each level. Must be 5 entries with last one 0 for unbound
+
+NDefines.NCountry.EVENT_PROCESS_OFFSET = 30
+NDefines.NFocus.MAX_SAVED_FOCUS_PROGRESS = 20
+NDefines.NAI.DIPLOMACY_SEND_EXPEDITIONARY_BASE = 0
+
+NDefines.NAir.AIR_WING_MAX_SIZE = 6400      
+
+NDefines.NDiplomacy.NUM_DAYS_TO_ENABLE_KICKING_NEW_MEMBERS_OF_FACTION = 14	-- down from 90 | Number of days before being able to kick a new member of faction 
+NDefines.NDiplomacy.NUM_DAYS_TO_ENABLE_REINVITE_KICKED_NATIONS = 14		-- down from 90 | Number of days before being able to re invite a kicked 
+NDefines.NDiplomacy.GUARANTEE_COST = 1000
+NDefines.NDiplomacy.REVOKE_GUARANTEE_COST = 1000
+NDefines.NDiplomacy.PEACE_SCORE_PER_PASS = 100.0						-- When you pass once you should get enough points to finish the peace deal
+NDefines.NDiplomacy.VOLUNTEERS_PER_COUNTRY_ARMY = 1				-- WAS 0.05 | Each army unit owned by the source country contributes this amount of volunteers to the limit.
+NDefines.NDiplomacy.VOLUNTEERS_DIVISIONS_REQUIRED = 0				-- WAS 30 | This many divisons are required for the country to be able to send volunteers.
+NDefines.NDiplomacy.FRONT_IS_DANGEROUS = 0
+NDefines.NDiplomacy.DIPLOMACY_HOURS_BETWEEN_REQUESTS = 12
+NDefines.NDiplomacy.MIN_TRUST_VALUE = -500 	
+
+NDefines.NProduction.MIN_POSSIBLE_TRAINING_MANPOWER = 5000000
+
+NDefines.NCountry.COUNTRY_SCORE_MULTIPLIER = 0				-- Weight of the country score.
+NDefines.NCountry.ARMY_SCORE_MULTIPLIER = 0					-- Based on number of armies.
+NDefines.NCountry.NAVY_SCORE_MULTIPLIER = 0					-- Based on number of navies.
+NDefines.NCountry.AIR_SCORE_MULTIPLIER = 0					-- Based on number of planes (which is typically a lot).
+NDefines.NCountry.INDUSTRY_SCORE_MULTIPLIER = 0				-- Based on number of factories.
+NDefines.NCountry.PROVINCE_SCORE_MULTIPLIER = 0				-- Based on number of controlled provinces.
+NDefines.NCountry.POPULATION_YEARLY_GROWTH_BASE = 0
+
+NDefines.NNavy.TRAINING_ACCIDENT_CHANCES = 0						-- down from 0.02 | Chances one ship get damage each hour while on training 		 0.0001	
+NDefines.NAir.AIR_WING_XP_TRAINING_MISSION_ACCIDENT_FACTOR = 0
+
+NDefines_Graphics.NAirGfx.MAX_MISSILE_BOMBING_SCENARIOS = 0
+NDefines_Graphics.NAirGfx.MAX_BOMBING_SCENARIOS = 0
+NDefines_Graphics.NAirGfx.MAX_PATROL_SCENARIOS = 0
+NDefines_Graphics.NAirGfx.MAX_DOGFIGHTS_SCENARIOS = 0
+NDefines_Graphics.NAirGfx.MAX_TRANSPORT_SCENARIOS = 0
+
+
+NDefines_Graphics.NGraphics.CAMERA_ZOOM_SPEED_DISTANCE_MULT = 20
+NDefines_Graphics.NGraphics.MAPICON_GROUP_STRATEGIC_SIZE = 1000
+NDefines_Graphics.NGraphics.MAP_ICONS_STATE_HUGE = 100
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_FIELD_COUNTRY_REFRESH = 30
+NDefines_Graphics.NGraphics.DECISION_MAP_ICON_DISTANCE_CUTOFF = 100
+NDefines_Graphics.NGraphics.NAVAL_COMBAT_DISTANCE_CUTOFF = 1200
+NDefines_Graphics.NGraphics.PROVINCE_ANIM_TEXT_DISTANCE_CUTOFF = 200
+NDefines_Graphics.NMapMode.MAP_MODE_TERRAIN_TRANSPARENCY = 0.8
+NDefines_Graphics.NGraphics.AIRBASE_ICON_DISTANCE_CUTOFF = 600
+NDefines_Graphics.NGraphics.NAVALBASE_ICON_DISTANCE_CUTOFF = 600
+NDefines_Graphics.NGraphics.RADAR_ICON_DISTANCE_CUTOFF = 150
+NDefines_Graphics.NAirGfx.AIRPLANES_ANIMATION_GLOBAL_SPEED_PER_GAMESPEED = {0.3, 0.3, 0.3, 0.3, 0.3, 0.3}
+NDefines_Graphics.NGraphics.DRAW_MAP_OBJECTS_CUTOFF = 1.0
+NDefines_Graphics.NGraphics.VICTORY_POINTS_DISTANCE_CUTOFF = {200, 350, 500}
+NDefines_Graphics.NGraphics.MAX_MESHES_LOADED_PER_FRAME = 5
+NDefines_Graphics.NMapMode.RADAR_ROTATION_SPEED = 0.0
+NDefines_Graphics.NMapMode.AIR_RANGE_INDICATOR_ROTATION_SPEED = 0.0000
+NDefines_Graphics.NAirGfx.AIRPLANES_SMOOTH_INTERPOLATION_MOVE = 0.02
+NDefines_Graphics.NAirGfx.AIRPLANES_SMOOTH_INTERPOLATION_TURN = 0.02
+NDefines_Graphics.NInterface.GRIDBOX_ELEMENTS_INTERPOLATION_SPEED = 0.2
+NDefines_Graphics.NGraphics.TREE_FADE_NEAR = 150.0
+NDefines_Graphics.NGraphics.TREE_FADE_FAR = 250.0
+NDefines_Graphics.NGraphics.RESOURCE_MAP_ICON_TEXT_CUTOFF = 400
+NDefines_Graphics.NGraphics.UNITS_DISTANCE_CUTOFF = 100
+NDefines_Graphics.NGraphics.SHIPS_DISTANCE_CUTOFF = 200
+NDefines_Graphics.NGraphics.UNIT_ARROW_DISTANCE_CUTOFF = 500
+NDefines_Graphics.NGraphics.UNITS_ICONS_DISTANCE_CUTOFF = 700
+NDefines_Graphics.NGraphics.NAVAL_COMBAT_DISTANCE_CUTOFF = 1000
+NDefines_Graphics.NGraphics.ADJACENCY_RULE_DISTANCE_CUTOFF = 1300
+NDefines_Graphics.NGraphics.LAND_COMBAT_DISTANCE_CUTOFF = 800
+NDefines_Graphics.NGraphics.SUPPLY_ICON_DISTANCE_CUTOFF = 1500
+NDefines_Graphics.NGraphics.PROV_CONSTRUCTION_ICON_DISTANCE_CUTOFF = 300
+NDefines_Graphics.NGraphics.STATE_CONSTRUCTION_ICON_DISTANCE_CUTOFF = 600
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_REFRESH_FREQ = 0.2
+NDefines_Graphics.NGraphics.MAPICON_GROUP_PASSES = 5
+NDefines_Graphics.NGraphics.DRAW_REFRACTIONS_CUTOFF = 0
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_FIELD_COUNTRY_REFRESH = 40
+NDefines_Graphics.NGraphics.WEATHER_DISTANCE_CUTOFF = 50
+NDefines_Graphics.NGraphics.MAP_ICONS_GROUP_CAM_DISTANCE = 100
+NDefines_Graphics.NGraphics.MAP_ICONS_STATE_GROUP_CAM_DISTANCE = 300
+NDefines_Graphics.NGraphics.MAP_ICONS_STRATEGIC_GROUP_CAM_DISTANCE = 400
+NDefines_Graphics.NGraphics.MAP_ICONS_STRATEGIC_AREA_HUGE = 250
+NDefines_Graphics.NGraphics.MAP_ICONS_COARSE_COUNTRY_GROUPING_DISTANCE = 300
+NDefines_Graphics.NGraphics.MAP_ICONS_COARSE_COUNTRY_GROUPING_DISTANCE_STRATEGIC = 0
+NDefines_Graphics.NGraphics.BLOOM_WIDTH = 0
+NDefines_Graphics.NGraphics.BLOOM_SCALE = 0
+NDefines_Graphics.NGraphics.BRIGHT_THRESHOLD = 0
+NDefines_Graphics.NGraphics.EMISSIVE_BLOOM_STRENGTH = 0
+NDefines_Graphics.NGraphics.DAY_NIGHT_FEATHER = 0.024
+NDefines_Graphics.NGraphics.DRAW_SHADOWS_CUTOFF = 0
+NDefines_Graphics.NGraphics.DRAW_SHADOWS_FADE_LENGTH = 0
+NDefines_Graphics.NGraphics.DRAW_FOW_CUTOFF = 0
+NDefines_Graphics.NGraphics.DRAW_FOW_FADE_LENGTH = 0
+
+NDefines.NTechnology.BASE_RESEARCH_POINTS_SAVED = 15
+
+NDefines.NBuildings.MAX_SHARED_SLOTS = 99 --max building slots in a state
+NDefines.NBuildings.MAX_BUILDING_LEVELS=20
+NDefines.NMilitary.TRAINING_ATTRITION = 0.01
+NDefines.NMilitary.MAX_NAVY_EXPERIENCE = 4999
+NDefines.NMilitary.MAX_AIR_EXPERIENCE = 4999
+NDefines.NMilitary.MAX_ARMY_EXPERIENCE = 4999
+
+NDefines.NOperatives.OPERATIVE_BASE_BOOST_IDEOLOGY = 0	
+NDefines.NCountry.MAX_PROPAGANDA_WAR_SUPPORT_IMPACT = 0		-- Max total penalty from operative performing the propaganda mission in a country
+NDefines.NCountry.PROPAGANDA_STABILITY_DAILY_DECAY = 0.002		-- Amount of stability recovered daily from propaganda effort
+NDefines.NResistance.RESISTANCE_ACTIVITY_MIN_GARRISON_PENETRATE_CHANCE = 0.1
+
+NDefines.NNavy.NAVAL_MINES_IN_REGION_MAX = 1	
+NDefines.NMilitary.BASE_DIVISION_BRIGADE_GROUP_COST = 0
+NDefines.NMilitary.BASE_DIVISION_BRIGADE_CHANGE_COST = 0
+NDefines.NMilitary.BASE_DIVISION_SUPPORT_SLOT_COST = 0
+NDefines.NNavy.PRIDE_OF_THE_FLEET_UNASSIGN_COST = 0	
+NDefines.NNavy.CONVOY_DEFENSE_MAX_CONVOY_TO_SHIP_RATIO = 20
+NDefines.NNavy.ADMIRAL_TASKFORCE_CAP = 20

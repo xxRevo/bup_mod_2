68128fe8a1f2b4bf30ee4fcafb77497539f27545
Berkem <berkem_elgul@hotmail.com>
2025-06-17 12:10:34 +0300

    DESC OKU
    -UK BSA companyi erken alabiliyor ve tufek/top cost azaltması veriyor, 1 ekstra top bonusu
    -GER hard/soft %10 -> %7.5
    -SOV move industry consumer -%5 -> -%40
    -SOV winter offensive de 40 division spawnlıyor
    -FRA kapitule olunca daha fazla tufek top ve sup ekipman
    -RAJ top bonusu ve ekstra focuslardan early mil
    -ITA afrikada itilirse tunusu alma focusu
    -Tankta armor reliability düşürmüyor
    -Marsa matruh 3 tile, cyreneica 4 tile
    -Bütün etiyopya tek air zone
    -Safari engellemek için railway ve supply hub değişiklikleri
    -Operation countenance, barbarossa baslayınca afrika ile raj arası kara baglantısı kurulabiliyor
    -UK italyaya 1940 14 ocakta savaş açabiliyor
    -ITA erken tank bonusu

--- common/defines/tole_defines.lua
+++ common/defines/tole_defines.lua
@@ -600,7 +600,7 @@ NDefines.NMilitary.XP_GAIN_FOR_SHATTERING = 0
 NDefines.NMilitary.MAX_DIVISION_SUPPORT_HEIGHT = 6 -- WAS 5 | We added one more support company as it feels necessary for quality tank or infantry divisions -Thrasymachus
 NDefines.NMilitary.MAX_DIVISION_BRIGADE_HEIGHT = 6		-- Max height of regiments in division designer
 NDefines.NMilitary.MAX_DIVISION_BRIGADE_WIDTH = 6
-NDefines.NMilitary.ORG_LOSS_FACTOR_ON_CONQUER = 0.4
+NDefines.NMilitary.ORG_LOSS_FACTOR_ON_CONQUER = 0.35
 
 NDefines.NSupply.RAILWAY_CONVERSION_COOLDOWN = 3 -- railways will be put on cooldown when they are captured by enemy and will not be usable during the cooldown
 NDefines.NSupply.RAILWAY_CONVERSION_COOLDOWN_CORE = 1
